3068c01a505568b2bb98968f8b929b6c04608adf
Gabriel Feo <gabriel@gabrielfeo.com>
2018-04-11 22:10:34 -0300

    Created the Stories Activity

---
+++ app/src/main/java/com/gfeo/yetanothernewsapp/StoriesActivity.java
@@ -0,0 +1,17 @@
+package com.gfeo.yetanothernewsapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gabrielfeo on 2018/04/11.
+ */
+
+public class StoriesActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_stories);
+	}
+}
