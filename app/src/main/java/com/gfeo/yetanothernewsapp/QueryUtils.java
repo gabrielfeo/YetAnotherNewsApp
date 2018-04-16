f95ad689d6779b92f9174966102797ae93aabf07
Gabriel Feo <gabriel@gabrielfeo.com>
2018-04-16 17:41:31 -0300

    [JavaDoc] QueryUtils.java

--- app/src/main/java/com/gfeo/yetanothernewsapp/QueryUtils.java
+++ app/src/main/java/com/gfeo/yetanothernewsapp/QueryUtils.java
@@ -23,12 +23,36 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.TimeZone;
 
-
+/**
+ * Utility class for querying <a href="http://open-platform.theguardian.com/">The Guardian's
+ * <i>Open Platform API</i></a>.
+ *
+ * @author gabrielfeo
+ */
 class QueryUtils {
 
+	/** A String containing the simple class name for {@link Log} methods */
 	private final static String LOG_TAG = QueryUtils.class.getSimpleName();
+	/**
+	 * A String setting a maximum number of news stories for the API to return
+	 *
+	 * @see QueryUtils#buildQueryUrl(String)
+	 */
 	private final static String pageSizeValue = "20";
 
+	/**
+	 * <p>Builds the query URL using a {@link Uri.Builder} and the
+	 * {@link QueryUtils#createUrl(String)} method.</p>
+	 * <p>The {@code Uri.Builder} appends query parameters that request the story headline and
+	 * byline specifically, and sets the <i>page-size</i> parameter according to the
+	 * {@link QueryUtils#pageSizeValue} field (this is the limit number of news stories to be
+	 * returned from the API).</p>
+	 *
+	 * @param sectionId the section ID value for the <i>section</i> parameter. This differs from
+	 *                  the <i>section name</i>; the ID of the Opinion section, for instance,
+	 *                  is "commentisfree".
+	 * @return the built query {@link URL}
+	 */
 	static URL buildQueryUrl(String sectionId) {
 		Uri.Builder uriBuilder = new Uri.Builder();
 		uriBuilder.scheme("https")
@@ -43,6 +67,12 @@ class QueryUtils {
 		return createUrl(uriBuilder.toString());
 	}
 
+	/**
+	 * Creates a new URL object from the given String URL.
+	 *
+	 * @param stringUrl a String holding a URL address
+	 * @return the created URL object
+	 */
 	private static URL createUrl(String stringUrl) {
 		URL url = null;
 		try {
@@ -53,6 +83,13 @@ class QueryUtils {
 		return url;
 	}
 
+	/**
+	 * Parses the UTC-format date and time String from the argument and reformats it to a custom
+	 * scheme, also converting it to the device's default time zone.
+	 *
+	 * @param utcDateString a String containing a UTC-formatted date and time
+	 * @return the reformatted and converted date and time String
+	 */
 	private static String parseUtcDate(String utcDateString) {
 		String dateString = utcDateString;
 		@SuppressLint("SimpleDateFormat") DateFormat utcDateFormat = new SimpleDateFormat
@@ -72,6 +109,12 @@ class QueryUtils {
 		return dateString;
 	}
 
+	/**
+	 * Makes an HTTP request to the given URL and returns a String as the response.
+	 *
+	 * @param url the URL for the HTTP request
+	 * @return a String of the JSON HTTP response
+	 */
 	static String makeHttpRequest(URL url) {
 		String jsonResponse = "";
 
@@ -113,7 +156,14 @@ class QueryUtils {
 		return jsonResponse;
 	}
 
-
+	/**
+	 * Converts the {@link InputStream} into a String which contains the
+	 * whole JSON response from the server.
+	 *
+	 * @param inputStream the {@code InputStream} to be read
+	 * @return the {@link BufferedReader} output from reading the
+	 * {@code InputStream}
+	 */
 	private static String readFromStream(InputStream inputStream) throws IOException {
 		StringBuilder output = new StringBuilder();
 		if (inputStream != null) {
@@ -129,7 +179,25 @@ class QueryUtils {
 		return output.toString();
 	}
 
-
+	/**
+	 * <p>Parses the JSON response returned by the
+	 * <a href="http://open-platform.theguardian.com/">The Guardian's <i>Open Platform
+	 * API</i></a> to an {@link ArrayList} of {@link Story} objects.</p>
+	 * <p>First, the {@code ArrayList} is cleared (if not empty already), so that the list items
+	 * aren't duplicated when this method is called on a list refresh. Then, a loop is started
+	 * and iterated according to the
+	 * {@link QueryUtils#pageSizeValue} field, thus precisely to the number of stories in the
+	 * response. The loop retrieves Strings from the JSON containing the story headline, author,
+	 * publication date and time, section name, and a link to the story page on the website,
+	 * namely, the fields of a {@code Story} object. Finally, each retrieved value, empty or
+	 * not, is used as an argument to the
+	 * {@link Story#Story(String, String, String, String, String)} constructor.</p>
+	 *
+	 * @param jsonResponseString the JSON response to be parsed, in a String object
+	 * @param storyArrayList     the {@code ArrayList<Story>} the JSON will
+	 *                           be parsed to
+	 * @return the fully populated {@code ArrayList<Story>}
+	 */
 	static ArrayList<Story> parseJsonToArrayList(String jsonResponseString,
 	                                             ArrayList<Story> storyArrayList) {
 
@@ -199,6 +267,15 @@ class QueryUtils {
 		return storyArrayList;
 	}
 
+	/**
+	 * Used in the first line in a catch block to check the exception argument message for a
+	 * certain keyphrase. If the exception corresponds to the permitted "No value for [{@code
+	 * key}]" {@link JSONException}, the exception will be disregarded and the caller method can
+	 * resume execution. Otherwise, another exception will be thrown.
+	 *
+	 * @param e the {@code JSONException} to be checked
+	 * @throws JSONException if the {@code JSONException} argument isn't of the allowed type
+	 */
 	private static void checkForPermittedJsonException(JSONException e) throws JSONException {
 		String permittedExceptionKeyphrase = "No value for";
 		if (!e.getMessage().contains(permittedExceptionKeyphrase)) {
