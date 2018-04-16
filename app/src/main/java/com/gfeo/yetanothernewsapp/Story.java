f8114570bc08df2ecdbcaa2f963ef22bb2a72a99
Gabriel Feo <gabriel@gabrielfeo.com>
2018-04-16 18:56:55 -0300

    [JavaDoc] Story.java

--- app/src/main/java/com/gfeo/yetanothernewsapp/Story.java
+++ app/src/main/java/com/gfeo/yetanothernewsapp/Story.java
@@ -3,17 +3,34 @@ package com.gfeo.yetanothernewsapp;
 import android.net.Uri;
 
 /**
- * Created by gabrielfeo on 2018/04/11.
+ * Represents a news story. Its state consists of the headline, date and time of publication, the
+ * section it belongs to, the author name, as well as the link to story on the website.
+ *
+ * @author gabrielfeo
  */
 
 class Story {
 
+	/** A String holding the story headline */
 	private final String mHeadline;
+	/** A String holding the date and time the story was published */
 	private final String mDateTime;
-	private final String mSection;
+	/** A String holding the story author name */
 	private final String mAuthor;
+	/** A String holding the name of the section the story belongs to */
+	private final String mSection;
+	/** A String holding a Uri to the story's page on the website */
 	private final Uri mLink;
 
+	/**
+	 * Sets the basic information about the story.
+	 *
+	 * @param headline A String holding the story headline
+	 * @param dateTime A String holding the date and time the story was published
+	 * @param author   A String holding the story author name
+	 * @param section  A String holding the name of the section the story belongs to
+	 * @param link     A String holding a Uri to the story's page on the website
+	 */
 	Story(String headline, String dateTime, String author, String section, String link) {
 		mHeadline = headline;
 		mDateTime = dateTime;
