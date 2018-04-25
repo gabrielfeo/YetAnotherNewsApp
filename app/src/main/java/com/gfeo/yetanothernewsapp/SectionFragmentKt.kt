15a4bbb79d8dd856ab51e1cf2aab207b1aec58cf
Gabriel Feo <gabriel@gabrielfeo.com>
2018-04-25 17:35:04 -0300

    [SectionFragmentKt] Translated onCreateView method

--- app/src/main/java/com/gfeo/yetanothernewsapp/SectionFragmentKt.kt
+++ app/src/main/java/com/gfeo/yetanothernewsapp/SectionFragmentKt.kt
@@ -2,8 +2,14 @@ package com.gfeo.yetanothernewsapp
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlin.properties.Delegates
 
 class SectionFragmentKt : Fragment() {
+
+    private var sectionPosition: Int by Delegates.notNull<Int>()
 
     companion object {
         @JvmStatic
@@ -15,6 +21,13 @@ class SectionFragmentKt : Fragment() {
             fragment.arguments = arguments
             return fragment
         }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View? = inflater?.inflate(R.layout.fragment_section, container, false)
+        sectionPosition = arguments.getInt("position")
+        TODO("call other initializer methods")
+        return view
     }
 
 }
