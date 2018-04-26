8b225ce12b82ea346221704eb72efced8e3bf900
Gabriel Feo <gabriel@gabrielfeo.com>
2018-04-26 13:53:04 -0300

    [SectionFragmentKt] Finished translating

--- app/src/main/java/com/gfeo/yetanothernewsapp/SectionFragmentKt.kt
+++ app/src/main/java/com/gfeo/yetanothernewsapp/SectionFragmentKt.kt
@@ -1,19 +1,27 @@
 package com.gfeo.yetanothernewsapp
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.widget.SwipeRefreshLayout
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
+import io.reactivex.Observable
+import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
+import java.net.URL
 import kotlin.properties.Delegates
 
 
 class SectionFragmentKt : Fragment() {
 
+    //TODO LOG_TAG constant
     private var sectionPosition: Int by Delegates.notNull()
     private lateinit var sectionId: String
     private lateinit var storyArrayAdapter: StoryArrayAdapter
@@ -91,6 +99,71 @@ class SectionFragmentKt : Fragment() {
             val intent = Intent(Intent.ACTION_VIEW, currentStory.link)
             if (intent.resolveActivity(activity.packageManager) != null) {
                 startActivity(intent)
+            }
+        }
+    }
+
+    private fun existsActiveNetworkConnection(): Boolean {
+        val cm: ConnectivityManager =
+                activity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo?
+        activeNetwork = cm.activeNetworkInfo
+        return (activeNetwork != null && activeNetwork.isConnectedOrConnecting)
+    }
+
+    private fun showProgressBar(view: View) {
+        val swipeRefreshLayout =
+                view.findViewById<SwipeRefreshLayout>(R.id.fragment_swiperefreshlayout)
+        if (swipeRefreshLayout.isRefreshing) return
+        view.findViewById<View>(R.id.fragment_textview_no_connection).visibility = View.GONE
+        view.findViewById<View>(R.id.fragment_swiperefreshlayout).visibility = View.GONE
+        view.findViewById<View>(R.id.fragment_progressbar).visibility = View.VISIBLE
+    }
+
+    private fun showStoriesList(view: View) {
+        val swipeRefreshLayout =
+                view.findViewById<SwipeRefreshLayout>(R.id.fragment_swiperefreshlayout)
+        if (swipeRefreshLayout.isRefreshing) swipeRefreshLayout.isRefreshing = false
+        view.findViewById<View>(R.id.fragment_textview_no_connection).visibility = View.GONE
+        view.findViewById<View>(R.id.fragment_progressbar).visibility = View.GONE
+        view.findViewById<View>(R.id.fragment_swiperefreshlayout).visibility = View.VISIBLE
+    }
+
+    private fun showNoConnectionView(view: View) {
+        val swipeRefreshLayout =
+                view.findViewById<SwipeRefreshLayout>(R.id.fragment_swiperefreshlayout)
+        if (swipeRefreshLayout.isRefreshing) swipeRefreshLayout.isRefreshing = false
+        view.findViewById<View>(R.id.fragment_swiperefreshlayout).visibility = View.GONE
+        view.findViewById<View>(R.id.fragment_progressbar).visibility = View.GONE
+        view.findViewById<View>(R.id.fragment_textview_no_connection).visibility = View.VISIBLE
+    }
+
+    private fun loadStories(storiesView: View) {
+        val storyArrayListObservable: Observable<ArrayList<Story>> = Observable.fromCallable {
+            val queryUrl: URL = QueryUtils.buildQueryUrl(sectionId)
+            val jsonResponse: String = QueryUtils.makeHttpRequest(queryUrl)
+            QueryUtils.parseJsonToArrayList(jsonResponse, storyArrayListArray[sectionPosition])
+        }
+    }
+
+    private fun initializeStoriesObserver(storiesView: View): Observer<ArrayList<Story>> {
+        return object : Observer<ArrayList<Story>> {
+            override fun onSubscribe(d: Disposable) {
+                if (existsActiveNetworkConnection()) {
+                    showProgressBar(storiesView)
+                    rxDisposable = d
+                }
+            }
+
+            override fun onNext(t: ArrayList<Story>) {}
+
+            override fun onError(e: Throwable) {
+                Log.e("SectionFragment", "Error on storyArrayListObservable", e)
+            }
+
+            override fun onComplete() {
+                showStoriesList(storiesView)
+                storyArrayAdapter.notifyDataSetChanged()
             }
         }
     }
